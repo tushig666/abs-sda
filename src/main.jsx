bd71fe5777fb8c3201da9820ff28725c2d456c11
tushig666 <tsetushig@gmail.com>
2025-06-13 15:49:25 +0900

    Update main.jsx: add Vercel Analytics import and ensure correct routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 import Project1 from './pages/Project1.tsx';
 import Project2 from './pages/Project2.tsx';
 import Project3 from './pages/Project3.tsx';
